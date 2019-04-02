12d7544da1a34df75cde04d1576591c867e0dc1b
Safa Bayar <admin@example.com>
2019-04-02 22:58:46 +0300

    Added new properties

---
+++ Sonradan Yaptiklarim/ders-9/okuma.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main(int argc,char * argv[]){
+
+  char buf[4096];
+  int c,po=0,z=0;
+  FILE * op = fopen(argv[1],"r");
+
+  while((c = getc(op)) != EOF){
+    printf("%c",c);
+
+    if(c == ' '){
+      po += 1;
+    }
+    z += 1;
+  }
+  printf("Kelime saysisi %d\n",po+1);
+  printf("Harf sayisi %d\n",z);
+  fclose(op);
+}
